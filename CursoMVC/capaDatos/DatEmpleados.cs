91f3d1d0af7ea57bd3e208ae11e12d04dd0c1423
constantino perez jimenez <cossystem@hotmail.com>
2019-07-08 15:52:26 -0500

    Se Actualizo la Capa de Entidad

--- CursoMVC/capaDatos/DatEmpleados.cs
+++ CursoMVC/capaDatos/DatEmpleados.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CapaEntidad;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
 namespace capaDatos
 {
     public class DatEmpleados
     {
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlconex"].ConnectionString);
+
+        public DataTable Listado()
+        {
+            SqlCommand cmd = new SqlCommand("sp_listar", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
     }
 }
